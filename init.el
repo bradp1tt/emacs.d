e26477a0cacd210a3806b46c8de2692e09180b50
John Patrick Amata <johnamata97@gmail.com>
2020-09-04 00:08:09 +0800

    letz go emacs baby

---
+++ init.el
@@ -0,0 +1,57 @@
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+'("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+(package-refresh-contents)
+(package-install 'use-package))
+
+(use-package try
+:ensure t)
+
+(use-package which-key
+:ensure t
+:config
+(which-key-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes (quote (wheatgrass)))
+ '(package-selected-packages
+   (quote
+    (which-key try use-package transpose-frame lsp-mode slime))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+;; make org-mode look pretty
+
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
+;; stuff
+
+;; global linum mode for line numbers
+(global-linum-mode)
+(set-default-font "Menlo 18")
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
