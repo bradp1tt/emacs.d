8348fbdc60e84c7c44dc8e3df9b448169a765a9f
John Patrick Amata <johnamata97@gmail.com>
2020-09-04 13:54:52 +0800

    swiper no swiping

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
+; remove default startup, change to scratch buffer
 (setq inhibit-startup-message t)
+
+;remove toolbar for more coding space
 (tool-bar-mode -1)
 
+; MELPA
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
@@ -33,7 +37,7 @@
  '(custom-enabled-themes (quote (wheatgrass)))
  '(package-selected-packages
    (quote
-    (tabbar which-key try use-package transpose-frame lsp-mode slime))))
+    (counsel ivy tabbar which-key try use-package transpose-frame lsp-mode slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -43,27 +47,58 @@
 
 
 ;; make org-mode look pretty
-
 (use-package org-bullets
   :ensure t
   :config
   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-;; better buffer info, directory in minibuffer, and autocomplete
-
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; buffer info and autocomplete - i left it for swiper search instead
+;;(setq ido-enable-flex-matching t)
+;;(setq ido-everywhere t)
+;;(ido-mode 1)
 
 ;; tabbar for buffer management
-
 (use-package tabbar
   :ensure t
   :config
   (tabbar-mode 1))
 
+;; windows management - moving
+(windmove-default-keybindings)
+
+
+;; swiper for default search tool and autocompletion
+;; not inbuilt from emacs. install "counsel" first from MELPA, not "ivy"  otherwise swiper wont be loaded
+(use-package counsel
+  :ensure t
+  )
+(use-package swiper
+  :ensure try
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "<f6>") 'ivy-resume)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    (global-set-key (kbd "<f1> l") 'counsel-load-library)
+    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+    ))
+
 ;; other stuff
-;; global linum mode for line numbers at the left
+
+;; global linum mode for line numbers
 (global-linum-mode)
 (set-default-font "Menlo 18")
 
